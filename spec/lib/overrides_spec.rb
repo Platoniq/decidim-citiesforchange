4f0cc3dd89524f49e49e57cb569d3521e2336c8f
Vera Rojman <vrojman@protonmail.com>
2021-02-23 18:43:54 +0100

    Prevent users from changing organization_colors

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -12,7 +12,8 @@ checksums = [
       # layouts
       "/app/views/layouts/decidim/_head_extra.html.erb" => "1b8237357754cf519f4e418135f78440",
       "/app/views/layouts/decidim/_logo.html.erb" => "2713715db652c8107f1fe5f2c4d618b6",
-      "/app/views/layouts/decidim/_mini_footer.html.erb" => "55a9ca723b65b8d9eadb714818a89bb3"
+      "/app/views/layouts/decidim/_mini_footer.html.erb" => "55a9ca723b65b8d9eadb714818a89bb3",
+      "/app/views/layouts/decidim/_organization_colors.html.erb" => "34f0d188a62108e7a57a1c270daed8bb"
     }
   },
   {
