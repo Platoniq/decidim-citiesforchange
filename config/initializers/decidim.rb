7f0f088c5a228011aa742adcbb80a94b044bfd40
Vera Rojman <vrojman@protonmail.com>
2021-04-02 12:29:27 +0200

    Add settings for content blocks

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -277,13 +277,31 @@ Decidim.configure do |config|
 
   Decidim.content_blocks.register(:homepage, :latest_blog_posts) do |content_block|
     content_block.cell = "decidim/content_blocks/latest_blog_posts"
+    content_block.settings_form_cell = "decidim/content_blocks/latest_blog_posts_form"
     content_block.public_name_key = "decidim.content_blocks.latest_blog_posts.name"
+
+    content_block.settings do |settings|
+      settings.attribute :title, type: :text, translated: true
+      settings.attribute :link_text, type: :text, translated: true
+      settings.attribute :link_url, type: :text, translated: true
+      settings.attribute :count, type: :integer, default: 3
+    end
+
     content_block.default!
   end
 
   Decidim.content_blocks.register(:homepage, :upcoming_meetings) do |content_block|
     content_block.cell = "decidim/content_blocks/upcoming_meetings"
+    content_block.settings_form_cell = "decidim/content_blocks/upcoming_meetings_form"
     content_block.public_name_key = "decidim.content_blocks.upcoming_meetings.name"
+
+    content_block.settings do |settings|
+      settings.attribute :title, type: :text, translated: true
+      settings.attribute :link_text, type: :text, translated: true
+      settings.attribute :link_url, type: :text, translated: true
+      settings.attribute :count, type: :integer, default: 3
+    end
+
     content_block.default!
   end
 end
