00353ffe87e9c6f0cd83ef4563f6e2e93bf9a02c
Ivan Vergés <ivan@platoniq.net>
2021-04-28 12:41:22 +0200

    upgrade decidim to .24 (#78)
    * upgrade decidim to .24
    
    * copyfy
    
    * overrides fixes
    
    * fix blockcontent cache key
    
    * fix upcoming meetings cellblock cache
    
    * readme
    
    * use html_truncate for reducing meetings text
    
    * change sender

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -5,7 +5,7 @@ Decidim.configure do |config|
   config.application_name = "Cities For Change Decidim"
 
   # The email that will be used as sender in all emails from Decidim
-  config.mailer_sender = "noreply@citiesforchange.org"
+  config.mailer_sender = "team@citiesforchange.org"
 
   # Sets the list of available locales for the whole application.
   #
