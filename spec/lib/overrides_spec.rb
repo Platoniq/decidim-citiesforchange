28c6cb55ebdae8e56c3496192066dc5dfad1abd7
Ivan Vergés <ivan@platoniq.net>
2021-05-07 16:36:10 +0200

    venues fix (#84)
    * email scss fixes
    
    * fix venues meetings

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -46,7 +46,8 @@ checksums = [
     package: "decidim-meetings",
     files: {
       # helpers
-      "/app/helpers/decidim/meetings/map_helper.rb" => "9f204fb413c5ae1e0c9ef276ab0ff3ba"
+      "/app/helpers/decidim/meetings/map_helper.rb" => "9f204fb413c5ae1e0c9ef276ab0ff3ba",
+      "/app/models/decidim/meetings/meeting.rb" => "92ec0cc608c342bfdd95eeeceb9cf749"
     }
   },
   {
