d76ac6e3ebd617c8f09780b444d4a105372992eb
Vera Rojman <vera@platoniq.net>
2021-07-19 19:57:25 +0200

    Degrowth.nl (#97)
    * Add email banner images for degrowth
    
    * WIP adapt styles
    
    * Move cititesforchange styles to theme
    
    * WIP styles
    
    * Restyle degrowth.nl instance
    
    * Restore files after git error
    
    * Lock organization colors + callout style
    
    * Restructure instance helper
    
    * Style emails
    
    * Use case insensitive matcher in system spec
    
    * Cleanup email styles

---
+++ config/initializers/instance_helper.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+Decidim::Organization.class_eval do
+  def instance_name
+    if citiesforchange?
+      :citiesforchange
+    elsif degrowth?
+      :degrowth
+    end
+  end
+
+  def instance_prefix_path(path)
+    if citiesforchange?
+      path
+    else
+      "#{instance_name}/#{path}"
+    end
+  end
+
+  def citiesforchange?
+    host.match?("citiesforchange")
+  end
+
+  def degrowth?
+    host.match?("degrowth") || host.match?("localhost") # DEBUG
+  end
+end
