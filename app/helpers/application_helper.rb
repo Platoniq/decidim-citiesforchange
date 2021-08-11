146d6ca0920aa3767087a61a1c6f6760658f0269
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2021-08-11 14:45:35 +0200

    wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def default_month?(months, month)
-    return current_month?(month) if months.include?(Time.current.beginning_of_month)
-
-    available_months = months.select(&:future?).presence || months
-    available_months.first.eql?(month)
-  end
-
-  def current_month?(month)
-    Time.current.beginning_of_month.eql?(month)
-  end
 end
