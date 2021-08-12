c0f5fa7f368f4c8afea4f8fe02c199205cc08345
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2021-08-12 17:37:57 +0200

    Fix remove nl locale

--- db/migrate/20210812141801_change_degrowth_organization_locales.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDegrowthOrganizationLocales < ActiveRecord::Migration[5.2]
-  def change
-    organization = Decidim::Organization.find_by(host: "degrowth")
-    attributes   = { available_locales: ["en"], default_locale: "en" }
-
-    organization&.update(attributes)
-  end
-end
